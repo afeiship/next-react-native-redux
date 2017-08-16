27df61d9d866fa2d8861dc947a7a2e8d2d8d475a
afeiship <1290657123@qq.com>
2017-08-16 14:57:07 +0800

    issue: inital

--- build/scripts.js
+++ build/scripts.js
@@ -9,13 +9,7 @@
   });
 
   gulp.task('scripts', function () {
-    return gulp.src('src/*.js')
-      .pipe(gulp.dest('dist'))
-      .pipe($.size({title: '[ default size ]:'}))
-      .pipe($.uglify())
-      .pipe($.rename({
-        extname: '.min.js'
-      }))
+    return gulp.src('src/**/*')
       .pipe(gulp.dest('dist'))
       .pipe($.size({title: '[ minimize size ]:'}));
   });
